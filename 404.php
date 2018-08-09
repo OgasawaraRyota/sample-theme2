10a958ee65ab5b13db0711805281eebacace4d7d
OgasawaraRyota <carp.nano8727@ezweb.ne.jp>
2018-08-09 21:00:23 +0900

    404ページ作成

--- 404.php
+++ 404.php
@@ -1,38 +1,9 @@
 <?php get_header(); ?>
 
-<nav id="slider">
-	<ul>
-		<li>
-			<img src="<?php echo get_template_directory_uri(); ?>/images/visual_image1.jpg" alt="一人ひとりに向き合った医療を" width="830" height="355">
-		</li>
-		<li>
-			<img src="<?php echo get_template_directory_uri(); ?>/images/visual_image2.jpg" alt="充実の検査を快適な空間でソライロ病院の人間ドック" width="830" height="355">
-		</li>
-		<li>
-			<img src="<?php echo get_template_directory_uri(); ?>/images/visual_image3.jpg" alt="看護師募集中" width="830" height="355">
-		</li>
-	</ul>
-</nav>
-
 <div class="contents">
 	<main>
 		<section class="topics">
-			<h2>トピックス</h2>
-			<ul>
-				<?php
-				$args = array(
-					'posts_per_page' => 5
-				);
-				query_posts( $args );
-				while( have_posts() ): the_post();
-				?>
-					<li>
-						<time datetime="<?php the_time( 'Y-m-d' ); ?>">
-							<?php the_time( get_option( 'date_format' ) ); ?></time>
-						<?php the_title(); ?>
-					</li>
-				<?php endwhile; ?>
-			</ul>
+			<h2>404 Not Found</h2>
 		</section>
 	</main>
 
